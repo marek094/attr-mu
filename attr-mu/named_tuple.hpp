d12ef43e3f8ea2b3d66c925297fed357e4c7b789
Marek Cerny <me@marekcerny.com>
2017-11-30 17:50:53 +0100

    assign./constructor move support

--- attr-mu/named_tuple.hpp
+++ attr-mu/named_tuple.hpp
@@ -35,14 +35,12 @@ namespace mu {
             return s;
         }
 
-        
+        // wrapper for PODs
         template<typename T> struct wrap {
             wrap() = default;
             wrap(T val) : val{val} { }
-            operator T&() & { return val; }
-            operator const T&() const& { return val; }
-            operator T&&() && { return val; }
-            operator const T&&() const&& { return val; }
+            operator T&() { return val; }
+            operator const T&() const { return val; }
             T val;
         };
         
@@ -50,11 +48,17 @@ namespace mu {
         using inheritable = typename std::conditional<std::is_class<T>::value, T, wrap<T>>::type;
     } // namespace impl;
     
+    
     template<impl::ull Data, typename T>
     struct attr : impl::inheritable<T> {
         using impl::inheritable<T>::inheritable;
+        
         constexpr attr() = default;
+        template<typename X>
+        constexpr attr(X&& v) : impl::inheritable<T>(std::forward<X>(v)) { }
+        
         static constexpr impl::ull str = Data;
+        static constexpr bool is_attr = true;
         
         T& value() & { return static_cast<impl::inheritable<T> &>(*this); }
         const T& value() const & { return static_cast<const impl::inheritable<T> &>(*this); }
@@ -63,27 +67,45 @@ namespace mu {
     };
     
     namespace impl {
-        constexpr inline impl::ull str_(const char *s) {
-            impl::ull res = 0;
+        constexpr inline ull str_(const char *s) {
+            ull res = 0;
             for (; *s; ++s) res = res*128 + *s % 128;
             return res;
         }
         
-        template<impl::ull, typename...>
+        template<ull, typename...>
         struct find { void operator()() {} };
         // Search
-        template<impl::ull S, impl::ull I, impl::ull... Is, typename T, typename... Ts>
-        struct find<S, mu::list<attr<I,T>, attr<Is,Ts>...>>
-        : find<S, mu::list<attr<Is,Ts>...> > {
-            using list_ = mu::list<attr<S,T>, attr<Is,Ts>...>;
+        template<ull S, ull I, typename T, typename... Ts>
+        struct find<S, list<attr<I,T>, Ts...>>
+        : find<S, list<Ts...> > {
+            using list_ = list<attr<S,T>, Ts...>;
             // Not found
-            using find<S, mu::list<attr<Is,Ts>...> >::operator();
+            using find<S, list<Ts...> >::operator();
             // Found
             auto&& operator ()(list_& list) { return list.val_.value(); }
             auto&& operator ()(const list_& list) { return list.val_.value(); }
             auto&& operator ()(list_&& list) { return list.val_.value(); }
             auto&& operator ()(const list_&& list) { return list.val_.value(); }
         };
+        
+//  TODO:
+//        template<typename...> struct assigner {};
+//        template<ull I, typename List, typename T, typename... As>
+//        struct assigner<list<attr<I, T>, As...>, List> {
+//            auto operator()(const List& from) {
+//                list<attr<I, T>, As...> res {
+//                    find<I, List>{}(from),
+//                    assigner<list<As...>>{}(from)
+//                };
+//
+////                impl::find<I, type_>{}(data_tupl_) = from.val_;
+////                assign_impl(static_cast<const list<Ts...>>(from));
+//                return res;
+//            }
+//        };
+
+        
     }
     
     template<typename... As>
@@ -91,11 +113,36 @@ namespace mu {
     private:
         using self_ = data<As...>;
         using type_ = mu::list<As...>;
+//        TODO:
+//        using type_ = mu::merge_sort_t<mu::list<As...>>;
+        static constexpr std::size_t size_ = sizeof...(As);
         
     public:
+        template<class = std::enable_if_t<(size_ > 0)>>
         constexpr data(As&&... values) : data_tupl_{std::forward<As>(values)...} { }
-        template<class = std::enable_if_t<(sizeof...(As) > 0)>>
+
+        template<typename...>
+        friend class data;
+        
         constexpr data() {}
+        
+        template<typename... Ts>
+        self_& operator= (const data<Ts...>& from) {
+            assign_impl(from.data_tupl_);
+            return *this;
+        }
+        
+        template<typename... Ts>
+        self_& operator= (data<Ts...>&& from) {
+            assign_impl(std::move(from.data_tupl_));
+            return *this;
+        }
+        
+        template<typename... AAs>
+        data(const data<AAs...>& p) { assign_impl(p.data_tupl_); }
+        
+        template<typename... AAs>
+        data(data<AAs...>&& p) { assign_impl(std::move(p.data_tupl_)); }
         
         template<impl::ull S>
         constexpr inline auto&& at() & {
@@ -130,10 +177,31 @@ namespace mu {
         friend constexpr inline auto&& get(const self_&& data) noexcept {
             return impl::find<S, type_>{}(std::move(data.data_tupl_));
         }
+        
+    private:
+        
+        // <Assign_impl> {
+        void assign_impl(const list<>&) { }
+
+        template<impl::ull I, typename T, typename... Ts>
+        void assign_impl(const list<attr<I, T>, Ts...>& from) {
+            impl::find<I, type_>{}(data_tupl_) = from.val_;
+            assign_impl(static_cast<const list<Ts...>>(from));
+        }
+
+        void assign_impl(list<>&&) {}
+        
+        template<impl::ull I, typename T, typename... Ts>
+        void assign_impl(list<attr<I, T>, Ts...>&& from) {
+            impl::find<I, type_>{}(data_tupl_) = std::move(from.val_);
+            assign_impl(static_cast<list<Ts...>>(std::move(from)));
+        }
+        // } </Assign_impl>
+     
     private:
         type_ data_tupl_;
     };
-    
+
     template<impl::ull... Is, typename... Ts>
     constexpr auto attr_mu(attr<Is,Ts>&&... args) {
         return data<attr<Is,Ts>...>(std::forward<attr<Is,Ts>>(args)...);
@@ -143,7 +211,6 @@ namespace mu {
     using named_tuple = data<As...>;
 
 } // namespace mu;
-
 
 constexpr auto operator"" _mu(const char* str, std::size_t) {
     return mu::impl::str_(str);
--- test/test.cpp
+++ test/test.cpp
@@ -129,7 +129,7 @@ TEST_CASE( "A mu::named_tuple<> tests", "[named_tuple]" ) {
     
     SECTION( "Move semantics const" ) {
         
-        const named_tuple<attr<"s"_mu, string>> ct {{"Long string"}};
+        const named_tuple<attr<"s"_mu, string>> ct {"Long string"};
         named_tuple<attr<"ss"_mu, string>> ct2;
         
         REQUIRE( get<"s"_mu>(ct) == "Long string");
@@ -140,5 +140,189 @@ TEST_CASE( "A mu::named_tuple<> tests", "[named_tuple]" ) {
         REQUIRE( get<"s"_mu>(ct) == "Long string");
         
     }
+    
+    SECTION( "Forwarding references - tuple") {
+        
+        int i = 9;
+        tuple<int&> t{i};
+        get<0>(t)++;
+        
+        REQUIRE( i == 10 );
+        
+        tuple<int&> t2{t};
+        get<0>(t2)++;
+        
+        REQUIRE( i == 11 );
+        
+        int j = 42;
+        tuple<int&> t3{j};
+        t3 = t2;
+        
+        REQUIRE( get<0>(t3) == 11 );
+        
+//        get<0>(t3)++;
+//
+//        REQUIRE( &i == &get<0>(t3) );
+//        REQUIRE(  i ==  get<0>(t3) );
+//        REQUIRE( i == 12 );
+//        REQUIRE( &get<0>(t2) == &get<0>(t3) );
+        
+    }
+        
+    SECTION( "Forwarding references - named_tuple") {
+        
+        int i = 9;
+        named_tuple<attr<""_mu, int&>> t{i};
+        get<""_mu>(t)++;
+        
+        REQUIRE( i == 10 );
+        
+        named_tuple<attr<""_mu, int&>> t2{t};
+        get<""_mu>(t2)++;
+        
+        REQUIRE( i == 11 );
+        
+        int j = 42;
+        named_tuple<attr<""_mu, int&>> t3{j};
+        
+        REQUIRE( j == 42 );
+        
+    }
 
+    SECTION( "Forwarding references - named_tuple cross_assign") {
+        
+        using st = named_tuple<attr<"r"_mu, int&>, attr<"u"_mu, unsigned>, attr<"d"_mu, double>>;
+        using nd = named_tuple<attr<"u"_mu, unsigned>, attr<"d"_mu, double>, attr<"r"_mu, int&>>;
+        
+        int ai = 10;
+        int bi = 20;
+        st a {ai, 1u, .1};
+        nd b {2u, .2, bi};
+        
+        get<"r"_mu>(a)++;
+        get<"r"_mu>(b)++;
+        
+        REQUIRE( ai == 11 );
+        REQUIRE( bi == 21 );
+        REQUIRE( &ai == &get<"r"_mu>(a) );
+        
+        a = b;
+// TODO:
+//        REQUIRE( &get<"r"_mu>(a) == &get<"r"_mu>(b) );
+//
+//        get<"r"_mu>(a)++;
+//        get<"r"_mu>(b)++;
+//
+//        REQUIRE( ai == 11 );
+//        REQUIRE( bi == 23 );
+    }
+    
+    SECTION( "Forwarding non-constructable" ) {
+        class F;
+        class K {
+        private:
+            friend class F;
+            K() : x{3} { }
+            K& operator=(const K&) { return *this; }
+            K(const K& c) : x{3} { }
+
+        public:
+            K(K&& c) : x{3} { }
+            K& operator=(K&&)  { return *this; }
+            int x;
+        };
+        
+        class F {
+        public:
+            static K get() {
+                return {};
+            }
+        };
+        
+        tuple<int, K> x {42 , F::get()};
+        
+//        auto z = x; <-- not poss
+        auto z = std::move(x);
+        
+        using tt_Ki = named_tuple<attr<"K"_mu, K>, attr<"i"_mu, int>>;
+        using tt_iK = named_tuple<attr<"i"_mu, int>, attr<"K"_mu, K>>;
+        
+        K cc = F::get();
+        attr<75, K> kk {std::move(cc)};
+
+        tt_iK ik {6, F::get()};
+        tt_Ki ki {attr<"K"_mu, K>{F::get()}, attr<"i"_mu, int>{9}};
+        
+//  TODO:
+//        tt_Ki xx = move(ik);//{attr<"K"_mu, K>{F::get()}, attr<"i"_mu, int>{9}};
+        
+        
+    }
+    
 }
+
+TEST_CASE("A mu::named_tuple<> constructor swaps") {
+    
+    // Point:
+    // tuple<attr<attr<"b"_mu, string>, "a"_mu,unsigned> v {};
+    // tuple<attr<"a"_mu,unsigned>, attr<"b"_mu, string> x = v
+    SECTION ( "Assing shufled and asymnetric assign" ) {
+    
+        named_tuple<attr<"b"_mu, string>, attr<"a"_mu, unsigned>> v {"onetwo", 34};
+        named_tuple<attr<"a"_mu,unsigned>, attr<"b"_mu, string>> w {12, "threefour"};
+        
+        v = w;
+        
+        named_tuple<attr<"a"_mu,unsigned>, attr<"b"_mu, string>> z = v;
+        
+        REQUIRE(v.at<"a"_mu>() == 12);
+        REQUIRE(v.at<"b"_mu>() == "threefour");
+        
+        REQUIRE(z.at<"a"_mu>() == 12);
+        REQUIRE(z.at<"b"_mu>() == "threefour");
+
+        named_tuple<
+        attr<"b"_mu, string>,
+        attr<"a"_mu, unsigned>,
+        attr<"default"_mu, string>
+        > x = v;
+
+        REQUIRE( x.at<"default"_mu>().empty() );
+        
+    }
+    
+    SECTION ( "Assing with unique_ptr, precise forwarding" ) {
+        named_tuple<attr<"ptr"_mu, unique_ptr<int>>, attr<"i"_mu, int>> ntp {new int(3), 4};
+        named_tuple<attr<"ptr"_mu, unique_ptr<int>>, attr<"i"_mu, int>> ntp2;
+        
+//        ntp2 = ntp; <= Non-move Error - OK
+        ntp2 = move(ntp);
+        
+        REQUIRE( * ntp2.at<"ptr"_mu>() == 3 );
+        
+        named_tuple<attr<"i"_mu, int>, attr<"ptr"_mu, unique_ptr<int>>> swaped_ntp;
+        swaped_ntp = move(ntp2);
+        
+        REQUIRE( * swaped_ntp.at<"ptr"_mu>() == 3 );
+    }
+    
+}
+
+TEST_CASE( "A mu::impl::assigner", "[mu::impl::assigner]" ) {
+    
+    using st = named_tuple<attr<"s"_mu, string>, attr<"u"_mu, unsigned>, attr<"d"_mu, double>>;
+    using nd = named_tuple<attr<"u"_mu, unsigned>, attr<"d"_mu, double>, attr<"s"_mu, string>>;
+    
+    st a {"a", 1u, .1};
+    nd b {2u, .2, "b"};
+    
+    st x {b};
+    
+    REQUIRE( x.at<"s"_mu>() == "b" );
+    
+    
+}
+
+
+
+
